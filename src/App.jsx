38bd071c74e7c8e222ca84fabf5bbb9adbaf85ae
Shaheer-Malik <shaheerm1223@gmail.com>
2023-08-31 14:55:57 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+import { useState } from 'react';
 
 function App() {
-
+  const [token, setToken] = useState(null);
 
   return (
     <>
-
+      <Authenticate token = {token} setToken = {setToken} />
+      <SignUpForm token = {token} setToken = {setToken} />
     </>
   )
 }
